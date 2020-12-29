6ba3e71ff875973dda66e56f02df6b7cb9e69c9d
lizhi <405628079@qq.com>
2020-12-29 21:13:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 import json
-print(111111)
 env_dist = os.environ
-env_dist["asclassname"]="myclassname"
-#print(env_dist)
 with open(env_dist.get("GITHUB_EVENT_PATH")) as json_file:
     data = json.load(json_file)
-    print(data)
+    code=data.comment.body
+    x = re.findall("public\s+class\s+(\S+)\s+" , code)
+    className=x[0]
+    f = open(data.comment.id+"/"+className+".as", "w")
+    f.write(code)
+    f.close()
+    
+    f = open(data.comment.id+"/main.txt", "w")
+    f.write(className)
+    f.close()
