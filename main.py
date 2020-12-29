2a4c244a73cbc8d6283b7783f1daed3b798aa747
lizhi <405628079@qq.com>
2020-12-29 17:44:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import json
 print(111111)
 env_dist = os.environ
+env_dist["asclassname"]="myclassname"
 #print(env_dist)
 with open(env_dist.get("GITHUB_EVENT_PATH")) as json_file:
     data = json.load(json_file)
