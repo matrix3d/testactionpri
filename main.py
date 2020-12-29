702ce3011624ad00fe04991f05c04d47791c5acd
lizhi <405628079@qq.com>
2020-12-29 21:31:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ with open(env_dist.get("GITHUB_EVENT_PATH")) as json_file:
     code=data["comment"]["body"]
     x = re.findall("public\s+class\s+(\S+)[\s{]" , code)
     className=x[0]
-    f = open(data["comment"]["id"]+"/"+className+".as", "w")
+    f = open(str(data["comment"]["id"])+"/"+className+".as", "w")
     f.write(code)
     f.close()
     
-    f = open(data["comment"]["id"]+"/main.txt", "w")
+    f = open(str(data["comment"]["id"])+"/main.txt", "w")
     f.write(className)
     f.close()
